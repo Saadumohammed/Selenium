8566c7321d8e6f0869543d6bfec735454bae9043
Saadumohammed <mohammedsadiq191@gmail.com>
2022-12-07 13:41:32 +0530

    m

--- src/Selenium2/TakesScreenshot.java
+++ src/Selenium2/TakesScreenshot.java
@@ -4,7 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -13,14 +15,18 @@ import com.google.common.io.Files;
 public class TakesScreenshot<FILE> {
 
 	public static void main(String[] args) throws IOException {
+		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ELCOT\\eclipse-workspace\\Selenium\\driver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.get("https://www.rahulshettyacademy.com/AutomationPractice/");
-		File src = ((org.openqa.selenium.TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		Files.copy(src, new File("C:\\Users\\ELCOT\\Downloads\\Selenium screenshot\\new.png"));
-	
+		TakesScreenshot shot = (TakesScreenshot) driver;
+		File src = shot.getScreenshotAs(OutputType.FILE);
+		File dest = new File("C:\\\\Users\\\\ELCOT\\\\Downloads\\\\Selenium screenshot\\\\new.png");
+		FileUtils.copyFile(src, dest);	
+//******************************************************************************************************
+		
 
 	}
 
